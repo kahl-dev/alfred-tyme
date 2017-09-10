7f252ad3a864bad13563961fc034fb3aaf963911
Patrick Kahl <Kahl.Patrick@googlemail.com>
2017-09-10 15:26:31 +0200

    add alfred-notifier

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 const alfy = require('alfy');
 const handler = require('./handler');
 const cache = require('./cache');
+const alfredNotifier = require('alfred-notifier');
 const index = { cache, handler };
 const [fn, action, value] = process.argv[2].split(':');
+
+alfredNotifier();
 
 // @TODO add export // external workflow
 // @TODO creat tasks from browser
